c4b7e786aa22f398bde9e14e21e74ad3e00ddbaa
Winnie0v0 <wzong@smith.edu>
2021-11-08 19:58:51 -0500

    click to execute

--- app/routes.py
+++ app/routes.py
@@ -38,20 +38,25 @@ def validate_image(stream):
 @app.route('/upload_file')
 def upload_index():
     files = os.listdir(app.config['UPLOAD_PATH'])
-    return render_template('upload_file.html', files=files)
+    return render_template('upload_files.html', files=files)
 
 @app.route('/upload_file', methods=['POST'])
 def upload_files():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return redirect(url_for('upload_file'))
+    if "SubmitBtn" in request.form:
+        uploaded_file = request.files['file']
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                    file_ext != validate_image(uploaded_file.stream):
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        return redirect(url_for('upload_files'))
+    if "GenerateBtn" in request.form:
+        return redirect(url_for('index'))
+    
 
-@app.route('/uploads/<filename>')
+
+@app.route('/upload/<filename>')
 def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH_2'], filename)
+    return send_from_directory(app.config['UPLOAD_PATH_2'], filename) 
